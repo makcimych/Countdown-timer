36e524b8b0d775c944a7ec5c50f638e7506d9cd7
Maksim <87930589+makcimych@users.noreply.github.com>
2024-05-31 21:36:43 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const targetDate = new Date('2024-05-30T16:55:33');
+const targetDate = new Date('2024-05-31T21:17:00');
 
 function updateCountdown() {
   const currentTime = new Date();
@@ -13,11 +13,18 @@ function updateCountdown() {
   document.getElementById("hours").innerText = hours;
   document.getElementById("minutes").innerText = minutes;
   document.getElementById("seconds").innerText = seconds;
-
-  if (difference < 0) {
-      clearInterval(interval);
-      document.getElementById("timer").innerText = "The event has started!";
+  
+  if (difference <= 0) {
+    clearInterval(interval);
+    document.getElementById("timer").innerText = "Вот и музон";
+    soundClick ();
   }
+}
+
+function soundClick () {
+  let audio = new Audio();
+  audio.src = 'D:\Study\WEB\Сountdown timer\src\Gio_Pika_MIRAVI_-_Mir_77823165.mp3';
+  audio.autoplay = true;
 }
 
 const interval = setInterval(updateCountdown, 1000);
